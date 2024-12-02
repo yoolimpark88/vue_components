089dc119ef63ddfbd549fe9144220b249c6230ee
이상민 <topgun8127@naver.com>
2024-12-02 21:11:55 +0900

    메인페이지 정리
    메인페이지 정리

--- src/layouts/PortalLayout.vue
+++ src/layouts/PortalLayout.vue
@@ -3,13 +3,7 @@
     <!-- GNB -->
     <sc-gnb />
     <h1>{{ fileName }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
+
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li>
