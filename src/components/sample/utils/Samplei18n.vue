17aa7bbf13e9f2406aa33e6affc998a4dce940ad
이상민 <topgun8127@naver.com>
2025-01-23 13:39:12 +0900

    sci18n 작업중

--- src/components/sample/utils/Samplei18n.vue
+++ src/components/sample/utils/Samplei18n.vue
@@ -1,3 +1,20 @@
 <template>
-  <div></div>
+  <div>
+    <sc-sample-box>
+      <template #description>
+        <div v-pre>
+          <p>DB에 데이터를 입력하고 {{ $t('메세지키') }}를 사용하여 다국어를 사용하니다.</p>
+          <p>this.$util.locale() = 현재 언어값 노출</p>
+        </div>
+      </template>
+      <sc-form-box col="2">
+        <sc-form-item label="MessageKey" span="1" required>
+          <sc-text-field v-model="messageKey" />
+        </sc-form-item>
+        <sc-form-item label="값" span="1">
+          <div v-html="$t(`${messageKey}`)"></div>
+        </sc-form-item>
+      </sc-form-box>
+    </sc-sample-box>
+  </div>
 </template>
